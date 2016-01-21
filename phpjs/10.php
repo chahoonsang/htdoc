fef8ec28ccaf3c3859ca0c2eb14e1779726cb639
차훈상 <chahoonsang@gmail.com>
2016-01-21 14:46:58 -0500

    done

--- phpjs/10.php
+++ phpjs/10.php
@@ -8,12 +8,24 @@
     <h1>JavaScript</h1>
     <script type="text/javascript">
       list = new Array("one","two","three");
-      document.write(list[1]);
-      document.write("test");
+      document.write(list[2]);
+      document.write(list.length);
+      i = 0;
+      while(i < list.length){
+        document.write(list[i]);
+        document.write("<br />");
+        i++;
+      }
     </script>
+    뭐지? 왜 안도지? 다시 또한번 test2 이상하다
+    또 안되쟎아 22
     <h1>PHP</h1>
     <?php
-
+        header("pragma: no-cache");
+      $list = array("one","two","three");
+      echo count($list);
+echo "ohyes";
+echo "why";
      ?>
   </body>
 </html>
